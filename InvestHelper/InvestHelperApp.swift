85b8c84fbd5d107be7390e124a9ff09b80b3ff5a
Sergey Dyatlov <sergeyd020501@gmail.com>
2025-10-30 20:32:27 +0300

    Initial Commit

---
+++ InvestHelper/InvestHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestHelperApp.swift
+//  InvestHelper
+//
+//  Created by Сергей Дятлов on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct InvestHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
